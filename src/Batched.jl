d66a3ebe8d860a16856221d3da1bdce75659d4d7
Roger-luo <hiroger@qq.com>
2018-11-05 23:21:31 -0500

    add CuArrays

--- src/Batched.jl
+++ src/Batched.jl
@@ -1,5 +1,6 @@
 module Batched
 
+using Requires
 # using LinearAlgebra
 
 include("BatchedArray.jl")
@@ -10,5 +11,9 @@ include("routines/blas.jl")
 include("routines/linalg.jl")
 
 include("matmul.jl")
+
+function __init__()
+    @require CuArrays = "3a865a2d-5b23-5a0f-bc46-62713ec82fae" include("cuda/cuda.jl")
+end
 
 end # module
