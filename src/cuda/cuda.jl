1fa13492e7e2259070b2f1d73ba25403191fa708
Roger-luo <hiroger@qq.com>
2018-11-05 23:34:44 -0500

    add cuda support

--- src/cuda/cuda.jl
+++ src/cuda/cuda.jl
@@ -1,1 +1,7 @@
-using CuArrays
+using CuArrays, CUDAdrv
+
+const CuBatchedArray{T, NI, N} = BatchedArray{T, NI, N, CuArray{T, N}}
+const CuBatchedMatrix{T, N} = CuBatchedArray{T, 2, N}
+const CuBatchedVector{T, N} = CuBatchedArray{T, 1, N}
+
+include("routines/blas.jl")
