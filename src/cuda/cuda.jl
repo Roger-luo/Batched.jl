c815b2110d5223c7192d54b0ed1d2143a6d18fbb
Roger-luo <hiroger@qq.com>
2018-11-06 03:40:12 -0500

    include matmul

--- src/cuda/cuda.jl
+++ src/cuda/cuda.jl
@@ -7,3 +7,4 @@ const CuBatchedVector{T, N} = CuBatchedArray{T, 1, N}
 const CuBatchedVecOrMat{T, N} = Union{CuBatchedMatrix{T, N}, CuBatchedVector{T, N}}
 
 include("routines/blas.jl")
+include("matmul.jl")
