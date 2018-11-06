8321f8d7d896473eced5eace36fd28cccd6a593f
Roger-luo <hiroger@qq.com>
2018-11-06 03:10:24 -0500

    fix args

--- src/cuda/routines/blas.jl
+++ src/cuda/routines/blas.jl
@@ -1,20 +1,20 @@
 import CuArrays.CUBLAS: @check, cublasop, cublasStatus_t, cublasHandle_t, cublasOperation_t, handle, libcublas
 
-function cublasSgemmStridedBatched(
-               handle, transA, transB,
-               m, n, k,
-               alpha,
-               A, lda, strideA,
-               B, ldb, strideB,
-               beta, C, ldc, strideC)
-
-  @check ccall((:cublasSgemmStridedBatched, libcublas),
-               cublasStatus_t,
-               (cublasHandle_t, cublasOperation_t, cublasOperation_t, Cint, Cint, Cint,
-                Ptr{Cfloat}, Ptr{Cfloat}, Cint, Cint, Ptr{Cfloat}, Cint, Cint, Ptr{Cfloat}, Ptr{Cfloat},
-                Cint, Cint),
-               handle, transa, transb, m, n, k, alpha, A, lda, strideA, B, ldb, strideB, beta, C, ldc, strideC)
-end
+# function cublasSgemmStridedBatched(
+#                handle, transA, transB,
+#                m, n, k,
+#                alpha,
+#                A, lda, strideA,
+#                B, ldb, strideB,
+#                beta, C, ldc, strideC)
+#
+#   @check ccall((:cublasSgemmStridedBatched, libcublas),
+#                cublasStatus_t,
+#                (cublasHandle_t, cublasOperation_t, cublasOperation_t, Cint, Cint, Cint,
+#                 Ptr{Cfloat}, Ptr{Cfloat}, Cint, Cint, Ptr{Cfloat}, Cint, Cint, Ptr{Cfloat}, Ptr{Cfloat},
+#                 Cint, Cint),
+#                handle, transa, transb, m, n, k, alpha, A, lda, strideA, B, ldb, strideB, beta, C, ldc, strideC)
+# end
 
 function batched_gemm!(transA::Char,
                transB::Char,
@@ -26,6 +26,9 @@ function batched_gemm!(transA::Char,
     m = size(A, transA == 'N' ? 1 : 2)
     k = size(A, transA == 'N' ? 2 : 1)
     n = size(B, transB == 'N' ? 2 : 1)
+
+    @assert size(A, 3) == size(B, 3) == size(C, 3) "Batch size mismatch"
+
     if m != size(C,1) || n != size(C,2) || k != size(B, transB == 'N' ? 1 : 2)
         throw(DimensionMismatch(""))
     end
@@ -38,13 +41,14 @@ function batched_gemm!(transA::Char,
     strideA = stride(A, 3)
     strideB = stride(B, 3)
     strideC = stride(C, 3)
+    batchCount = size(A, 3)
     @check ccall((:cublasSgemmStridedBatched, libcublas), cublasStatus_t,
                  (cublasHandle_t, cublasOperation_t,
                   cublasOperation_t, Cint, Cint, Cint, Ptr{Float32},
                   Ptr{Float32}, Cint, Cint, Ptr{Float32}, Cint, Cint, Ptr{Float32},
-                  Ptr{Float32}, Cint, Cint),
+                  Ptr{Float32}, Cint, Cint, Cint),
                  handle(), cutransA,
                  cutransB, m, n, k, [alpha], A, lda, strideA, B, ldb, strideB, [beta],
-                 C, ldc, strideC)
+                 C, ldc, strideC, batchCount)
     C
 end
