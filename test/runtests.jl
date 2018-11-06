09bd566232f3fec34e67b696eefdaf7fdc745fe2
Roger-luo <hiroger@qq.com>
2018-11-06 17:16:00 -0500

    add test & benchmark

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,26 +1,3 @@
-using Batched, BenchmarkTools
+using Test
 
-A = rand(10, 10, 100)
-C = zeros(10, 10, 100)
-@benchmark Batched.gemm!('N', 'N', 1.0, A, A, 1.0, C)
-
-using LinearAlgebra
-
-@which tr(rand(10, 10))
-
-function test_tr(A::Matrix{T}) where T
-    n = BLAS.checksquare(A)
-    t = zero(T)
-    @inbounds for i=1:n
-        t += A[i,i]
-    end
-    t
-end
-
-
-X = rand(10000, 10000)
-@benchmark tr(X)
-
-
-
-@benchmark test_tr(X)
+include("routines/gemm.jl")
