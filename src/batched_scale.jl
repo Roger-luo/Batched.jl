05a1b726afaad4cefbfecbcde410bb08fcb4ed71
Roger-luo <hiroger@qq.com>
2018-11-13 15:11:38 -0500

    make batched scale and transpose contiguous

--- src/batched_scale.jl
+++ src/batched_scale.jl
@@ -25,7 +25,8 @@ batched_mul!(Y::AbstractArray{T, 3}, A::AbstractArray{T, 3}, B::BatchedScaleMatr
 function batched_mul!(Y::AbstractArray{T, 3}, A::BatchedScaleMatrix{K, T}, B::BatchedTransposeOrAdjoint{T, 3, <:AbstractArray{T, 3}}) where {T, K}
     @boundscheck size(A, 3) == size(B, 3) == size(Y, 3) || error("Batch size mismatch")
 
-    @inbounds for k in size(B, 3), j in size(B, 2), i in size(B, 1)
+    # NOTE: we exchange the order of i,j since this is the contiguous order in memory
+    @inbounds for k in size(B, 3), i in size(B, 1), j in size(B, 2)
         C[i, j, k] = A.scalars[k] * B[i, j, k]
     end
     C
