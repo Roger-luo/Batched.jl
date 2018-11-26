7f34a69b8399f151463d7d7422395fe332a8a97d
Roger-luo <hiroger@qq.com>
2018-11-25 21:48:26 -0500

    rm build

--- src/Batched.jl
+++ src/Batched.jl
@@ -2,10 +2,6 @@ module Batched
 
 using BatchedRoutines
 
-const ext = joinpath(dirname(@__DIR__), "deps", "ext.jl")
-isfile(ext) || error("Batched.jl has not been built, please run Pkg.build(\"Batched\").")
-include(ext)
-
 include("abstractarray.jl")
 include("adjtrans.jl")
 include("batched_scale.jl")
